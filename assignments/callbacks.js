eabb2d120b77a3a934a9fa078600288b072aa138
Triston Armstrong <triston95strong@gmail.com>
2019-04-02 17:09:15 -0500

    finished stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -65,4 +65,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+
+  //Instructions dont make sense
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -18,14 +18,44 @@ closOne();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function () {
+    return count += 1;
+  }
 };
+let newCounter = counter();
+console.log(newCounter())
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+ 
+ const countObj = {
+  increment: function (){
+    let count = 0;
+    return function () {
+      return count += 1;
+    }
+  },
+  decrement: function (){
+    let count = 0;
+    return function () {
+      return count -= 1;
+    }
+  }
+}
+  
+return countObj;
+  
 };
+let countObject = counterFactory();
+let increment = countObject.increment();
+let decrement = countObject.decrement();
+
+console.log(increment())
+console.log(increment())
+
+console.log(decrement())
+console.log(decrement())
